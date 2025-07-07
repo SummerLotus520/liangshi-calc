2461bb72230cfdbca074d7114d30fc0b79c9c4d3
SummerLotus520 <2402804043@qq.com>
2025-07-07 15:14:52 +0800

    [Fix]自动添加特效模板

--- .github/scripts/addEffectTemplates.cjs
+++ .github/scripts/addEffectTemplates.cjs
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const rootDirs = [
-  'liangshi-gs',
-  'liangshi-sr'
+  'damage/liangshi-gs',
+  'damage/liangshi-sr'
 ]
 
 const gsTemplate = `{
